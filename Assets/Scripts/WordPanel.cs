c18284cd198371318bfa3f72905d776062f79f33
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-04-17 21:10:18 +0200

    Initial commit

---
+++ Assets/Scripts/WordPanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordPanel : MonoBehaviour {
+    public GameObject letterPanelPrefab;
+
+    // Start is called before the first frame update
+    void Start() {
+        SpawnLetterPanels();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SpawnLetterPanels() {
+        
+    }
+}
